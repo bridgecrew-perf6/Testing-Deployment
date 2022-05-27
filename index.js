f6809717dde7940006729c8024c5d54a5f39c49e
Prabhjyot <knowprabhjyot@gmail.com>
2022-05-27 09:20:56 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+    res.send("ENDPOINTS");
+})
+
+app.get('/api/v1/user', (req, res) => {
+    const user = {
+        name: "Prabh",
+        age: 27
+    }
+
+    return res.status(200).json({
+        data: user,
+        message: "Succesfully fetched the user!"
+    })
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})

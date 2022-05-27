04b8446e2f654800686dd23bef2f38795b90e7b2
Prabhjyot <knowprabhjyot@gmail.com>
2022-05-27 09:35:44 -0700

    added react application

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+function App() {
+  const [user, setUser] = useState();
+
+  const getUser = async () => {
+    const response = await axios.get("http://localhost:4000/api/v1/user");
+    console.log(response);
+    setUser(response.data.data);
+  };
+
+  return (
+    <>
+      <section>
+        <div> Name: {user?.name}</div>
+        <div>Age: {user?.age}</div>
+      </section>
+      <button onClick={getUser}>Get User!</button>
+    </>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
+const cors = require('cors');
+
+// This is a middle ware again that tells us where our UI files are 
+app.use(express.static('build'))
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("ENDPOINTS");
